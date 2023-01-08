ae3d529289cd3c9e98d26754c95b0079aefa902b
kimseonguk197 <ksg39412@naver.com>
2023-01-08 17:07:10 +0900

    test commit

--- src/main/java/com/example/board/RedisConfig.java
+++ src/main/java/com/example/board/RedisConfig.java
@@ -10,7 +10,7 @@
 //import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 //import org.springframework.data.redis.serializer.StringRedisSerializer;
 //import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-///
+//
 //@Configuration
 //public class RedisConfig {
 //    @Value("${spring.redis.host}")
