76c69296192d748645d4be404efe7c0f1fc80c35
kimseonguk197 <ksg39412@naver.com>
2023-03-18 09:11:33 +0900

    security config 수정

--- src/main/java/com/example/board/SecurityConfig.java
+++ src/main/java/com/example/board/SecurityConfig.java
@@ -10,9 +10,20 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+//@EnableWebSecurity어노테이션을 붙여줘야 Securiry를 활성화 시킬 수있따.
+//depricated된 사항은 spring-data-jpa 레포 참고
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //    암호화 모듈 객체 생성
+//    Component
+//    @component은 setter나 builder 등을 통해서 사용자가 프로퍼티를 변경해서 생성한 인스턴스를 스프링에게 관리하라고 맡기는 것,
+//    1)개발자가 직접 컨트롤이 가능한내부 클래스에 사용
+//    2)class에서만 사용이 가능
+//    Bean
+//    @bean는 클래스를 스프링한테 알아서 인스턴스 생성한 후에 등록(bean으로) 하라고 맡기는 것 . -> Spring IoC 컨테이너(또는 DI 컨테이너)에 의해 생성 및 관리
+//    1)개발자가 컨트롤이 불가능한 외부 라이브러리 사용시 사용
+//    2)매서드에 붙일 수 있다. 클래스 위에는 @Configuration을 붙여줘야 한다.(객체를 생성할 때 싱글톤을 보장하기 위해)
+
     @Bean
     public PasswordEncoder passwordEncoder(){
 //        암호화 모듈 리턴
